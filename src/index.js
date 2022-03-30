95ceee83697e98edada824c1604581250ab87306
some-6ude <102697068+some-6ude@users.noreply.github.com>
2022-03-30 14:20:18 -0600

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+let btn = document.createElement("button");
+btn.innerHTML = "run my awsome code!";
+btn.onclick = function () {
+  //variables
+  var test = prompt("what do you want to write");
+  var test3 = test + " ";
+  var test2 = "the text thats gonna get written is " + test;
+  //lazy basic js code
+  alert(test2);
+  alert(test);
+  alert("made by karim");
+  document.write(test);
+  document.write("\n");
+  console.log("made by karim :)");
+  document.title = test;
+};
+document.body.appendChild(btn);
